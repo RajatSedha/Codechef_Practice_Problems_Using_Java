423c391a49e4b78aabfca3d4c122be418a9f10da
Rajat Sedha <80527626+RajatSedha@users.noreply.github.com>
2021-03-13 09:38:47 +0530

    Create NumberMirror.java

---
+++ Beginners/NumberMirror.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(num);
+	}
+}
