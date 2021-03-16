1d746890f06dbb1381eff8207e93ce22efdb3087
Rajat Sedha <80527626+RajatSedha@users.noreply.github.com>
2021-03-16 12:17:10 +0530

    Create FindRemainder.java

---
+++ Beginners/FindRemainder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int i = 1;
+        while(i<=n){
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            int sum = b%c;
+            System.out.println(sum);
+            i = i+1;
+        }
+    }
+}
